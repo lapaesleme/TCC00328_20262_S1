aece5c4082adbdec9234f7f509058bef372b7e03
juliamonteiro-max <juliamonteiro@id.uff.br>
2026-09-14 11:34:21 -0300

    ex3

--- src/main/java/uff/ic/lleme/tcc00328/trabalhos/julia/Ex3/data/Turma.java
+++ src/main/java/uff/ic/lleme/tcc00328/trabalhos/julia/Ex3/data/Turma.java
@@ -5,5 +5,9 @@ package uff.ic.lleme.tcc00328.trabalhos.julia.Ex3.data;
  * @author psmjs
  */
 public class Turma {
+
+    public void setProfessor(Professor la) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
