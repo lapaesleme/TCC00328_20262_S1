45ee92721bbc3990dbd09da82a79ee605ef08070
juliamonteiro-max <juliamonteiro@id.uff.br>
2026-09-21 10:16:15 -0300

    ex4

--- src/main/java/uff/ic/lleme/tcc00328/trabalhos/julia/Ex4/Jogador.java
+++ src/main/java/uff/ic/lleme/tcc00328/trabalhos/julia/Ex4/Jogador.java
@@ -1,12 +1,18 @@
 package uff.ic.lleme.tcc00328.trabalhos.julia.Ex4;
 
 public class Jogador {
-    private String id;
     private String nome;
     
-    public Jogador (String id, String nome) {
-        this.id = id;
+    public Jogador (String nome) {
+        this.nome = nome;    
+    } 
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
         this.nome = nome;
-        
-    }    
+    }
+    
 }
